38f5271dc2a5d442c35269583968d355ff2bca5d
Eshak <43699326+bdeshak@users.noreply.github.com>
2023-04-21 16:36:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ const day = date.getDate();
    }
   }
  
- const expire_mess = "<p style='color:red;'>Note: Your Password Accessing Expire Date Will be End:"+user.expire_date;
+ const expire_mess = "<p style='color:red;'>Note: Your Password Accessing Expire Date Will be End:</p>"+user.expire_date;
  function member_password_access(){
   if(user.expire_date!==fulldate){
    return expire_mess;
