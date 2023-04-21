871e3690f27bbbdc922c457c4a88317248a2847c
Eshak <43699326+bdeshak@users.noreply.github.com>
2023-04-21 16:49:01 +0600

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const decryptedPass = cryptr.decrypt(ecrypted_text);
 });
 
 var myArray = [
-    {"id": 1, "name": "admin", "email": "mdalonebd@gmail.com","premium_member":1,"expire_date":"2023-4-22"},
+    {"id": 1, "name": "admin", "email": "mdalonebd@gmail.com","premium_member":1,"expire_date":"2023-4-11"},
 
     {"id": 2, "name": "momin","email":"momin0132813@gmail.com","premium_member":1,"expire_date":"2023-4-22"},
     {"id": 43, "name": "ebrahim", "email": "sh2471386@gmail.com","premium_member":1,"expire_date":"2023-4-22"}
